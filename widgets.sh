7e8d4e584d78eb6ed7c679af840521c7c3bd869a
Dienes Viktor <dienesviktor@gmail.com>
2022-07-11 21:42:02 +0200

    The behavior of widget.sh is controlled by the WIDGET_CONTROL environmental variable

--- widgets.sh
+++ widgets.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
-./mem.sh
-./proc.sh
-./user.sh
-./disk.sh
+chmod 755 *
+
+export WIDGET_CONTROL="mem, cpu, proc, user, disk, battery"
+
+if [ -z ${WIDGET_CONTROL+x} ];
+  then
+      ./mem.sh
+      echo ""
+      ./cpu.sh
+  else
+      mem="mem"
+      cpu="cpu"
+      proc="proc"
+      user="user"
+      disk="disk"
+      battery="battery"
+      if [[ "$WIDGET_CONTROL" == *"$mem"* ]]; then
+          ./mem.sh
+          echo ""
+      fi
+      if [[ "$WIDGET_CONTROL" == *"$cpu"* ]]; then
+          ./cpu.sh
+          echo ""
+      fi
+      if [[ "$WIDGET_CONTROL" == *"$proc"* ]]; then
+          ./proc.sh
+          echo ""
+      fi
+      if [[ "$WIDGET_CONTROL" == *"$user"* ]]; then
+          ./user.sh
+          echo ""
+      fi
+      if [[ "$WIDGET_CONTROL" == *"$disk"* ]]; then
+          ./disk.sh
+          echo ""
+      fi
+      if [[ "$WIDGET_CONTROL" == *"$battery"* ]]; then
+          ./battery.sh
+          echo ""
+      fi
+  fi
