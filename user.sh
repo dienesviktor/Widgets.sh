e191c978e6e54e25f88650ede2e33cbcacd67e33
Dienes Viktor <dienesviktor@gmail.com>
2022-07-11 19:13:36 +0200

    Write a script that displays the number of non-system users and the number of user groups

--- user.sh
+++ user.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+non_system_users() {
+    FILE="/etc/passwd"
+    counter=0
+    while IFS=: read -r username password userid groupid comment homedir cmdshell
+    do
+      #echo "$username, $userid, $comment $homedir"
+      if [ "$userid" -ge 1000 ]
+      then
+          ((counter++))
+      fi
+    done < $FILE
+    echo $counter
+}
+
+main() {
+    echo "Non-system users: $(non_system_users)"
+    echo "Groups: $(groups | wc -w)"
+}
+
+main
